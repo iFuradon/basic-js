51776b4a811a6a02330a7d5d5f5d0330979bd2ed
iFuradon <iFuradon@gmail.com>
2024-12-23 21:59:44 +0400

    feat: add carbon-dating functionality

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,12 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const activity = parseFloat(sampleActivity); 
+  if (isNaN(activity) || activity <= 0 || activity > MODERN_ACTIVITY || typeof sampleActivity !== 'string') return false; 
+  const k = 0.693 / HALF_LIFE_PERIOD; 
+  const t = Math.log(MODERN_ACTIVITY / activity) / k; 
+  return Math.ceil(t);
 }
 
 module.exports = {
